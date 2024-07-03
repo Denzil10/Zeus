9f21f4e9ddfcdec48758bf1af7b04ca4b83d62fa
denzil <2020.denzil.puvathingal@ves.ac.in>
2024-07-03 19:45:42 +0530

    save token on server env

--- app.py
+++ app.py
@@ -224,15 +224,7 @@ def oauth2callback():
     flow.fetch_token(authorization_response=authorization_response)
 
     credentials = flow.credentials
-    session['credentials'] = {
-        'token': credentials.token,
-        'refresh_token': credentials.refresh_token,
-        'token_uri': credentials.token_uri,
-        'client_id': credentials.client_id,
-        'client_secret': credentials.client_secret,
-        'scopes': credentials.scopes
-    }
-
+    print(credentials)
     save_credentials(credentials)
     return redirect(url_for('register'))
 

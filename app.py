6fbd3e38bbb088c54ccd491873171c11dca86ced
denzil <2020.denzil.puvathingal@ves.ac.in>
2024-07-05 12:30:40 +0530

    refresh fix in auth

--- app.py
+++ app.py
@@ -248,7 +248,7 @@ def authorize():
 
     authorization_url, state = flow.authorization_url(
         access_type='offline',
-        include_granted_scopes='true'
+        include_granted_scopes='true',
         prompt='consent'
     )
 

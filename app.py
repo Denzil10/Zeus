c943b21fbdab71f38a0c6cc7623044fae1e7ac6e
Denzil10 <91799448+Denzil10@users.noreply.github.com>
2024-08-06 11:49:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ else:
     print("OAuth client secrets not found")
     client_secrets = None
 
-SCOPES = ['https://www.googleapis.com/auth/contacts','https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile', 'openid']
+SCOPES = ['https://www.googleapis.com/auth/contacts','https://www.googleapis.com/auth/userinfo.email', 'https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/admin.reports.audit.readonly', 'openid']
 
 # Function to extract user identifier
 def get_user(query):
